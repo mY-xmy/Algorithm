25eaa14e68434f6d5ecab7f6dfa713c3ac6c3c65
Xu Mingyu <592882665@qq.com>
2021-11-10 23:38:26 +0800

    add KNN

--- ML.py
+++ ML.py
@@ -4,11 +4,14 @@
 @FilePath: ML.py
 @Author: Xu Mingyu
 @Date: 2021-11-03 23:21:55
-@LastEditTime: 2021-11-04 22:59:04
+@LastEditTime: 2021-11-10 23:38:06
 @Description: 
 @Copyright 2021 Xu Mingyu, All Rights Reserved. 
 """
 import numpy as np
+import pandas as pd
+import torch
+import pdb
 
 def convolution(inputs, filters, padding=(0,0), stride=(1,1)):
     """
@@ -19,17 +22,19 @@ def convolution(inputs, filters, padding=(0,0), stride=(1,1)):
     @param {*} stride
     @return {*}
     """
-    assert inputs.shape[0] == filters.shape[1]
+    assert inputs.shape[1] == filters.shape[1]
+    inputs = np.pad(inputs, ((0,0), (0,0), (padding[0], padding[0]), (padding[1], padding[1])))
+
     N, C, H, W = inputs.shape
     D, C, h, w = filters.shape
 
-    output = np.zeros(N, D, ((H-h) // stride[0] + 1, (W-w) // stride[1] + 1))
+    output = np.zeros((N, D, (H-h) // stride[0] + 1, (W-w) // stride[1] + 1))
     inputs = np.expand_dims(inputs, 1) # size(N, 1, C, H, W)
     filters = np.expand_dims(filters, 0) # size(1, D, C, H, W)
 
     for i in range(output.shape[2]):
         for j in range(output.shape[3]):
-            output[:, :, i, j] = np.sum(inputs[:, i * stride[0]: i * stride[0] + h, j * stride[1]: j * stride[1] + w] * filters, axis=(2,3,4))
+            output[:, :, i, j] = np.sum(inputs[:, :, :, i * stride[0]: i * stride[0] + h, j * stride[1]: j * stride[1] + w] * filters, axis=(2,3,4))
             
     return output
 
@@ -53,5 +58,56 @@ def convolution_backword(d_output, input, filters, padding=(0,0), stride=(1,1)):
             grad[:, :, i, j] = np.sum(d_output * input[:, i: H * stride[0]+i: stride[0], j: W * stride[1]+j: stride[1]], axis=(2,3)) # size(D, 1, H, W) * size(C, H, W)
             
     return grad
+
+def cosine_dist(x,y):
+    m, n = x.size(0), y.size(0)
     
+    norm_x = x.norm(p=2, dim=1, keepdim=True).expand(m,n)
+    norm_y = y.norm(p=2, dim=1, keepdim=True).expand(n,m).t()
+    dist = torch.matmul(x, y.t()) / (norm_x * norm_y)
+    return dist
     
+def KNN(embeddings, chunk = 1024, k=50):
+    topK_index= []
+    topK_distance = []
+
+    n = (embeddings.size(0) + chunk - 1) // chunk
+    with torch.no_grad():
+        for i in range(n):
+            a = i*chunk
+            b = min((i+1)*chunk, embeddings.size(0))
+            x = embeddings[a:b]
+            y = embeddings
+            dist = cosine_dist(x,y)
+
+            dist_desc = torch.sort(dist, descending=True)
+            topK_idx, topK_dist = dist_desc[1][:, :k], dist_desc[0][:, :k] 
+            topK_index.append(topK_idx)
+            topK_distance.append(topK_dist)
+
+            del x, y, dist, dist_desc
+            torch.cuda.empty_cache()
+
+    topK_index = torch.cat(topK_index, axis=0)
+    topK_distance = torch.cat(topK_distance, axis=0)
+   
+    return topK_index, topK_distance
+    
+def test():
+    image = np.array([[[0,0,1,0,2],[1,0,2,0,1],[1,0,2,2,0],[2,0,0,2,0],[2,1,2,2,0]],\
+                  [[2,1,2,1,1],[2,1,2,0,1],[0,2,1,0,1],[1,2,2,2,2],[0,1,2,0,1]],\
+                  [[2,1,1,2,0],[1,0,0,1,0],[0,1,0,0,0],[1,0,2,1,0],[2,2,1,1,1]]])
+    image = image.reshape(1, *image.shape)
+
+    f = np.array([[[[-1,0,1],[0,0,1],[1,-1,1]],[[-1,0,1],[1,-1,1],[0,1,0]],[[-1,1,1],[1,1,0],[0,-1,0]]],\
+                    [[[0,1,-1],[0,-1,0],[0,-1,1]],[[-1,0,0],[1,-1,0],[1,-1,0]],[[-1,1,-1],[0,-1,-1],[1,0,0]]]])
+
+    padding = (1,1)
+    stride = (2,2)
+
+    res = convolution(image, f, padding, stride)
+    print(res.shape)
+    print(res)
+
+if __name__ == "__main__":
+    test()
